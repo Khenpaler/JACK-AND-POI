1886c130ef94d0344b8caf8be5dac69ee5a60231
Kent <asilo.kentbryanv19@gmail.com>
2023-05-07 10:59:07 +0800

    ADDED COMMENTS & TIE SCENARIO EXTRA FEATURE

--- script.js
+++ script.js
@@ -1,36 +1,55 @@
-const buttons = document.querySelectorAll('button');
-const playerScore = document.getElementById('player-score');
-const computerScore = document.getElementById('computer-score');
-const result = document.getElementById('result');
+const buttons = document.querySelectorAll("button");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const result = document.getElementById("result");
 
-let score = {player: 0, computer: 0};
+let score = { player: 0, computer: 0 };
 
 function computerPlay() {
-  const choices = ['rock', 'paper', 'scissors'];
+  // RANDOM CHOICE OF THE COMPUTER
+  const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
+// ASSIGNING THE CHOICE OF THE PLAYER AND THE COMPUTER
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const playerChoice = button.id;
+    const computerChoice = computerPlay();
+    playRound(playerChoice, computerChoice);
+  });
+});
+
 function playRound(playerChoice, computerChoice) {
+  // SAME CHOICE SCENARIO
   if (playerChoice === computerChoice) {
-    return 'It\'s a tie!';
-  } else if (
-    (playerChoice === 'rock' && computerChoice === 'scissors') ||
-    (playerChoice === 'paper' && computerChoice === 'rock') ||
-    (playerChoice === 'scissors' && computerChoice === 'paper')
+    result.textContent = `You're Tied! Computer also chose ${computerChoice}.`;
+  }
+
+  // PLAYER WINNER SCENARIO
+  else if (
+    (playerChoice === "rock" && computerChoice === "scissors") || // CONDITIONS FOR PLAYER TO BE WINNER
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
   ) {
     score.player++;
     playerScore.textContent = score.player;
+    // CONDITION FOR IF PLAYER IS THE FINAL WINNER
     if (score.player === 5) {
-      result.textContent = 'Congratulations! You won the game!';
+      result.textContent = "Congratulations! You won the game!";
       disableButtons();
       return;
     }
     result.textContent = `You Win! ${playerChoice} beats ${computerChoice}.`;
-  } else {
+  }
+
+  // COMPUMTER WINNER SCENARIO
+  else {
     score.computer++;
     computerScore.textContent = score.computer;
+    // CONDITION FOR IF COMPUTER IS THE FINAL WINNER
     if (score.computer === 5) {
-      result.textContent = 'Sorry, you lost the game.';
+      result.textContent = "Sorry, you lost the game.";
       disableButtons();
       return;
     }
@@ -43,11 +62,3 @@ function disableButtons() {
     button.disabled = true;
   });
 }
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const playerChoice = button.id;
-    const computerChoice = computerPlay();
-    playRound(playerChoice, computerChoice);
-  });
-});
